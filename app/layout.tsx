833a6a5f0af45044e16b98889e1f15dd1dd71d07
Sheel Ganvir <sheelganvir2805@gmail.com>
2025-04-22 20:06:27 +0530

    Remove tested login/signup buttons

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,15 +33,7 @@ export default function RootLayout({
     }}>
       <html lang="en">
         <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
-        <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <SignedOut>
-              <SignInButton />
-              <SignUpButton />
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </header>
+        
           {children}
         </body>
       </html>
