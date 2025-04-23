f3b172caec70a7cf1c0983e1c9e75d4ea2035cd6
Sheel Ganvir <sheelganvir2805@gmail.com>
2025-04-23 07:05:46 +0530

    implement clerk webhooks

--- types/index.d.ts
+++ types/index.d.ts
@@ -5,8 +5,8 @@ declare type CreateUserParams = {
   clerkId: string;
   email: string;
   username: string;
-  firstName: string;
-  lastName: string;
+  firstName: string | null; // Allow null
+  lastName: string | null;  // Allow null
   photo: string;
 };
 
